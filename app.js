8a0feeef401472b83bab742cedb83d3797e9bb27
Naldo Habib <naldohabib@gmail.com>
2020-07-19 17:49:38 +0700

    Add CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require('cors');
 
 //+ library
 const methodOverride = require("method-override");
@@ -26,6 +27,8 @@ const adminRouter = require("./routes/admin");
 const apiRouter = require("./routes/api");
 
 var app = express();
+
+app.use(cors);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
